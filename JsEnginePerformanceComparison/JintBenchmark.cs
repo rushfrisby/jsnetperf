3a0fd557b45ec27357834b8ae4fb86d36bdf0515
Marko Lahma <marko.lahma@finbiosoft.com>
2018-01-07 11:31:49 +0200

    make Jint tolerant and ignore Loc

--- JsEnginePerformanceComparison/JintBenchmark.cs
+++ JsEnginePerformanceComparison/JintBenchmark.cs
@@ -1,12 +1,21 @@
-﻿namespace JsEnginePerformanceComparison
+﻿using Esprima;
+
+namespace JsEnginePerformanceComparison
 {
     public class JintBenchmark : EngineBenchmark
     {
         private static readonly Jint.Engine jintEngine = new Jint.Engine();
 
+        private static readonly ParserOptions parserOptions = new ParserOptions
+        {
+            AdaptRegexp = true,
+            Tolerant = true,
+            Loc = false
+        };
+
         protected override void RunScript(string script)
         {
-            jintEngine.Execute(script);
+            jintEngine.Execute(script, parserOptions);
         }
     }
 }
